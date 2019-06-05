d8d7f6f33aadbb7a66eab6daf68f12d51e72a810
Darlin Regla <darling-0205@outlook.com>
2019-06-05 12:30:48 -0400

    ejercicio

--- javascript_101/ejercicio.js
+++ javascript_101/ejercicio.js
@@ -8,3 +8,15 @@ console.log(edad);
 document.write(`<h1>${nombre}</h1>`);
 document.write(`<p>${edad}</p>`);
 
+//2.hacer que el usuario digite
+//su ciudad, barrio y direccion
+//imprimirlo en ub <div>
+
+var nombre = prompt("digite su ciudad");
+va barrio =  prompt("digite su barrio");
+var direccion = prompt("digite su direccion");
+
+document.write(`<div>${ciudad}</div>`);
+document.write(`<div>${barrio}</div>`);
+document.write(`<div>${direccion}</div>`);
+
